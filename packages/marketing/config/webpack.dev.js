02690ebb1097e41cf42faae44487b2dabb43af46
Robin Salomon <rsalomon@teksystems.com>
2022-07-19 12:03:18 -0400

    Copy and pasted from other repo

--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -1,3 +1,4 @@
+
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
