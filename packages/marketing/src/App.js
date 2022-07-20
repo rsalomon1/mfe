ecd46cc7027a2d8ab55ba6991c7f2882af568743
Robin Salomon <rsalomon@teksystems.com>
2022-07-20 11:56:46 -0400

    Made a change to the marketing App.js

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import { StylesProvider } from '@material-ui/core/styles';
 import Landing from './components/Landing';
